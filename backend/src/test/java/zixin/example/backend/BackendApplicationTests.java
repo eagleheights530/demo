f47c760a040eb2d59cb4c7adc43f6fb23d71a919
Zixin <czx0530@gmail.com>
2023-09-11 17:33:49 -0700

    Remove backend test

--- backend/src/test/java/zixin/example/backend/BackendApplicationTests.java
+++ backend/src/test/java/zixin/example/backend/BackendApplicationTests.java
@@ -1,12 +1,13 @@
 package zixin.example.backend;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+// @SpringBootTest
 class BackendApplicationTests {
 
-	@Test
+	// @Test
 	void contextLoads() {
 	}
 
